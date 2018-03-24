fb3b0a4acbdcdf5ef4918653163b8ec7d8abb199
Keith Gerhards <keith.gerhards@gmail.com>
2018-03-24 17:21:06 -0600

    put facebook iniit in main.js

--- JS/main.js
+++ JS/main.js
@@ -1,5 +1,25 @@
 $(document).ready(function(){
 
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '421844468260973',
+      cookie     : true,
+      xfbml      : true,
+      version    : 'v2.12'
+    });
+      
+    FB.AppEvents.logPageView();   
+      
+  };
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "https://connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+
    FB.getLoginStatus(function(response) {
        statusChangeCallback(response);
    });
