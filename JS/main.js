62e5dbb95c68b70e6a1b3abe68680374eeb99292
Keith Gerhards <keith.gerhards@gmail.com>
2018-03-24 18:37:58 -0600

    fix facebook stuff

--- JS/main.js
+++ JS/main.js
@@ -9,7 +9,10 @@ $(document).ready(function(){
     });
       
     FB.AppEvents.logPageView();   
-      
+    FB.getLoginStatus(function(response) {
+        statusChangeCallback(response);
+    });
+
   };
 
   (function(d, s, id){
@@ -20,19 +23,25 @@ $(document).ready(function(){
      fjs.parentNode.insertBefore(js, fjs);
    }(document, 'script', 'facebook-jssdk'));
 
-   FB.getLoginStatus(function(response) {
-       statusChangeCallback(response);
-   });
 
 });
 
 
 function statusChangeCallback(response) {
    console.log(response);
+        if (response.status === 'connected') {
+            // the user is logged in and has authenticated your
+            // app, and response.authResponse supplies
+            // the user's ID, a valid access token, a signed
+            // request, and the time the access token
+            // and signed request each expire
+            var uid = response.authResponse.userID;
+            var accessToken = response.authResponse.accessToken;
+        } else if (response.status === 'not_authorized') {
+            // the user is logged in to Facebook,
+            // but has not authenticated your app
+        } else {
+            // the user isn't logged in to Facebook.
+        }
 }
 
-function checkLoginState() {
-  FB.getLoginStatus(function(response) {
-    statusChangeCallback(response);
-  });
-}
